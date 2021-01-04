1d937e0c8078b39cf7a0bfe79882dee4cb17a62f
dcorso21 <david.l.corson@gmail.com>
2021-01-03 23:07:37 -0500

    api and browser working

--- src/components/App.vue
+++ src/components/App.vue
@@ -11,12 +11,24 @@
     </div>
     <h2>Your Visitor Id is: {{ visitorId }}</h2>
     <button v-on:click="callServerAPI">Get Visit History</button>
-    <h3>{{responseSummary}}</h3>
-    <pre>{{ serverData }}</pre>
+    <h3>{{ responseSummary }}</h3>
+    <pre class="response">{{ serverData }}</pre>
   </div>
 </template>
 
 <script>
+import FingerprintJS from "@fingerprintjs/fingerprintjs-pro";
+
+// If you would like to hide your API keys, you can put them in a .env file.
+// You can also simply whitelist the addresses from which you will be making the calls in your user dashboard
+// For the server API key, we reccommend setting up an Auth Header instead, you can read more in the docs:
+// https://dev.fingerprintjs.com/docs/server-api
+const BROWSERAPIKEY = "P7vQXeD8G1VlmDAwwFir";
+const SERVERAPIKEY = "9MLmWT6ziZqePf9JrDZP";
+
+// This will limit the amount of visits the api queries
+const limitTo = 10;
+
 export default {
   name: "app",
   data: function () {
@@ -28,14 +40,29 @@ export default {
   },
   // When mounted, this vue instance will look up FPJS
   mounted: function () {
-    this.visitorId = "123456789";
+    FingerprintJS.load({
+      token: BROWSERAPIKEY,
+    })
+      .then((fp) => fp.get())
+      .then((result) => {
+        this.visitorId = result.visitorId;
+      });
   },
   methods: {
-    // When the button is clicked, this function will 
+    // When the button is clicked, this function will
     // run and get visitor history from the server
     callServerAPI: function () {
-      this.responseSummary = "Received history of last visits:";
-      this.serverData = "hello there";
+      fetch(
+        `https://api.fpjs.io/visitors/${this.visitorId}?limit=${limitTo}&token=${SERVERAPIKEY}`
+      )
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          this.responseSummary = `Received history of ${data.visits.length} visits:`;
+          data = JSON.stringify(data.visits, null, 4);
+          this.serverData = data;
+        });
     },
   },
 };
@@ -91,6 +118,13 @@ code::after {
 
 .logo {
   animation: spin 4s 1s infinite linear;
+}
+
+.response {
+  font-family: inherit;
+  text-align: left;
+  margin: 5%;
+  font-size: 14px;
 }
 
 @keyframes spin {
